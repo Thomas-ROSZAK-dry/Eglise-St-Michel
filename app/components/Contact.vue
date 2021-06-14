2509e87782b4e6fe7eed556ba4bab457312748a2
TROSZAK <thomasroszak81@gmail.com>
2021-06-14 21:35:58 +0200

    wip

---
+++ app/components/Contact.vue
@@ -0,0 +1,132 @@
+<template>
+ <div class="background"  >
+  <div class="style-form" >
+    <form  v-on:submit.prevent="checkForm" class="form" >
+    
+    <div class="form-control" :class="{invalid: userNameValidity ==='invalid'}" >
+      <label for="Votre nom">Votre Nom</label>
+    <input type="text" id="user-name" name="user-name" v-model.trim="userName"/>
+    </div>
+    <p class="errormessage" v-if="userNameValidity ==='invalid'" >Merci de saisir votre nom</p>
+
+    <div class="form-control" :class="{invalid: emailValidity ==='invalid'}" >
+      <label for="Votre adresse Mail">Votre email</label>
+     <input type="text" id="email" name="email" v-model.trim="email"/>
+    </div>
+    <p class="errormessage" v-if="emailValidity ==='invalid'" >Merci de saisir votre email</p>
+  
+   <div class="form-control" :class="{invalid: sujetValidity ==='invalid'}" >
+  <label for="Sujet">Sujet</label>
+    <input type="text" id="sujet" name="sujet" v-model.trim="sujet">
+    </div>
+ <p class="errormessage" v-if="sujetValidity ==='invalid'" >Merci de saisir le champs sujet</p>
+
+    <div class="form-control" :class="{invalid: textValidity ==='invalid'}" >
+    <textarea name="sujet" id="" cols="30" rows="10" v-model.trim="text" >
+    </textarea>
+    </div>
+    <p class="errormessage" v-if="textValidity ==='invalid'" >Merci de saisir votre demande</p>
+ 
+    <div>
+    <button> Envoyer</button>
+    </div>
+
+  </form>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      userName:'',
+      email: '',
+      sujet:"",
+      text:"",
+      errorList: [],
+      userNameValidity:'pending',
+      emailValidity:'pending',
+      sujetValidity:'pending',
+      textValidity:'pending',
+    }
+  },
+  methods: {
+    checkForm() {
+        if (this.userName === '') {
+         this.userNameValidity='invalid';
+       } else{
+         this.userNameValidity='valid';
+       }
+        if (this.email === '') {
+         this.emailValidity='invalid';
+       } else{
+         this.emailValidity='valid';
+       }
+        if (this.sujet === '') {
+         this.sujetValidity='invalid';
+       } else{
+         this.sujetValidity='valid';
+       }
+         if (this.text === '') {
+         this.textValidity='invalid';
+       } else{
+         this.textValidity='valid';
+       }
+}
+}
+}
+</script>
+
+<style scoped>
+@import '../assets/scss/_formulaire';
+
+.form-control.invalid input{
+  border-color: grey;
+}
+.errormessage{
+  color: grey;
+  font-weight: 700;
+}
+.background {
+  background:lightgrey;
+  background:contain;
+  height: 100vh;
+  width: 100vw;
+  padding-top:20px
+}
+input {
+  display:flex;
+  margin-bottom:25px;
+  border-radius:10px;
+  width: 15vw;
+  height: 2vw;
+}
+textarea{
+  margin-top:15px;
+  border-radius:10px;
+  width:15vw;
+  height: 25vh;
+}
+.style-form{
+  display:flex;
+  margin:0;
+  padding:180px;
+  margin: auto;
+  max-width: 40rem
+}
+.textarea:not([rows]) {
+    max-height: 60em;
+    min-height: 30em;
+   
+}
+button {
+  margin-top: 10px;
+  width:15vw;
+  border-radius:10px;
+  height: 5vh;
+  font-size:x-large
+  
+}
+
+</style>
---
+++ app/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="title is-ancestor " >
+    
+  <div >
+    <div class="title">
+            <div class="">
+        <article class="title is-child ">
+          <p class="title has-text-centered"><router-link :to= "{name: 'Evenements'}" >Evènements</router-link></p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus, iusto molestias obcaecati debitis sed hic veritatis perferendis, ut error, sapiente numquam cum culpa tempore? Rerum, numquam pariatur voluptate vero earum repellat perferendis culpa illo quae saepe dignissimos sit porro ab corporis deserunt facilis sint ullam quia iste. Amet, corrupti hic.</p>
+        </article>
+        <article class="title is-child ">
+          <p class="title has-text-centered"><router-link :to="{name: 'ContactDeux'}">Contact</router-link></p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores cupiditate labore excepturi aut fugiat illo est possimus temporibus dignissimos non.</p>
+        </article>
+      </div>
+      <div class="title is-parent is-vertical">
+        <article class="title is-child ">
+          <p class="title has-text-centered "><router-link :to= "{name:'StMichel'}"> St Michel</router-link></p>
+         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus, iusto molestias obcaecati debitis sed hic veritatis perferendis, ut error, sapiente numquam cum culpa tempore? Rerum, numquam pariatur voluptate vero earum repellat perferendis culpa illo quae saepe dignissimos sit porro ab corporis deserunt facilis sint ullam quia iste. Amet, corrupti hic.</p>
+        </article>
+        <article class="title is-child ">
+          <p class="title has-text-centered "><router-link :to= "{name:'Images'}"> Galerie d'images </router-link></p>
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores cupiditate labore excepturi aut fugiat illo est possimus temporibus dignissimos non.</p>
+        </article>
+      </div>
+      
+    </div>
+   
+ </div>
+</div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+@import '../assets/scss/_home';
+
+
+.title {
+ font-family: "Raleway ExtraBold", sans-serif;
+}
+</style>
